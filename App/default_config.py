5980f258395e89eeb981f366c18ea75bf47e24f7
TheLegend0100010 <125493340+TheLegend0100010@users.noreply.github.com>
2024-04-02 01:12:41 +0000

    UPDATE DATABASE URI

--- App/default_config.py
+++ App/default_config.py
@@ -1,2 +1,2 @@
-SQLALCHEMY_DATABASE_URI = "sqlite:///temp-database.db"
+SQLALCHEMY_DATABASE_URI = "postgres://info_2602_project_db_user:5PPCKgF118KwYSME8QkQpnogBY3sDB4e@dpg-co5g4iuv3ddc7390l8hg-a/info_2602_project_db"
 SECRET_KEY = "secret key"
