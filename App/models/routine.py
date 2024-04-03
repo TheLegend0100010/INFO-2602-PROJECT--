085708f08fb6b72ba10c1205b814e5dd86cff20f
TheLegend0100010 <125493340+TheLegend0100010@users.noreply.github.com>
2024-04-03 19:14:50 +0000

    .

--- App/models/routine.py
+++ App/models/routine.py
@@ -3,5 +3,5 @@ from App.database import db
 class Routine(db.Model):
     id = db.Column(db.Integer, primary_key = True, autoincrement = True)
 
-    def __init__():
+    def __init__(self):
         print(1)
