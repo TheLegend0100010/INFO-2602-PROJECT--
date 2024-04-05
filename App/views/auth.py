a8d466d6917b8e194ca5ea5763f3f3e8072408ce
TheLegend0100010 <125493340+TheLegend0100010@users.noreply.github.com>
2024-04-05 00:50:06 +0000

    profile page

--- App/views/auth.py
+++ App/views/auth.py
@@ -30,7 +30,7 @@ def identify_page():
 def login_action():
     data = request.form
     token = login(data['username'], data['password'])
-    response = redirect(request.referrer)
+    response = redirect(url_for('profile_page'))
     if not token:
         flash('Bad username or password given'), 401
     else:
