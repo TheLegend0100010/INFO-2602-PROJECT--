287940bafd5d9f9ff9f60d38174f7cff35652e0d
TheLegend0100010 <125493340+TheLegend0100010@users.noreply.github.com>
2024-04-13 15:12:46 +0000

    more testing

--- App/views/index.py
+++ App/views/index.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, redirect, render_template, request, send_from_directory, jsonify
 from flask_jwt_extended import jwt_required
-from App.models import db
+from App.models import Exercise
 from App.controllers import create_user, create_routine, create_workout, create_exercise
+from App.database import db
 import json
 
 index_views = Blueprint('index_views', __name__, template_folder='../templates')
@@ -31,14 +32,23 @@ def init():
     create_user('rick', 'rickpass')
     with open('exercises.json', encoding = "ISO-8859-1") as file:
         exercises = json.load(file)
-        return jsonify(lenght=len(exercises['exercises']))
+        records =[]
+        # return jsonify(lenght=len(exercises['exercises']))
         for exercise in exercises['exercises']:
             image = exercise['name'].replace(' ', '_')
             imagelink = f"https://raw.githubusercontent.com/wrkout/exercises.json/master/exercises/{image}/images/0.jpg"
             # instructions = ""
             # for instruct in exercise['instructions']:
             #     instructions += instruct
-            create_exercise(exercise['name'], exercise['level'], exercise['primaryMuscles'][0], imagelink)
+            record = Exercise(exercises['name'], exercises['level'], exercises['primaryMuscles'][0], imagelink)
+            records.append(record)
+            # records.append({'name': exercise['name'],
+            # 'level': exercise['level'],
+            # 'muscle': exercise['primaryMuscles'][0],
+            # 'image': imagelink})
+        db.session.bulk_save_objects(records)
+        db.session.commit()
+            # create_exercise(exercise['name'], exercise['level'], exercise['primaryMuscles'][0], imagelink)
     create_routine()
     create_workout()
     return jsonify(message='db initialized!')
